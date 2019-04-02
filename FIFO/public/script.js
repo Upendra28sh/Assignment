66bb3ae8eb0efa5e736d51604f130ed38a7bf64c
upendra <upendra861@gmail.com>
2019-04-02 23:46:08 +0530

    Update

--- FIFO/public/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('input[type="radio"]').click(function() {
-    if ($(this).attr("id") == "Specific-Tokens") {
-      $("#Specific-Tokens-Div").show();
-    } else {
-      $("#Specific-Tokens-Div").hide();
-    }
-  });
-});
